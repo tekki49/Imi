3c418e48a4102bf4a7e2679edc0228216543d62e
Sai <saikrishna@beehyv.com>
2015-10-07 18:00:16 +0530

    changing the name of imi util to json util

--- src/main/java/com/imi/rest/util/ImiJsonUtil.java
+++ src/main/java/com/imi/rest/util/ImiJsonUtil.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ImiUtil {
+public class ImiJsonUtil {
 	
 	private static ObjectMapper objectMapper = new ObjectMapper();
 
@@ -19,7 +19,6 @@ public class ImiUtil {
 	}
 
 	public static String getJSONString(String key, String value) throws JsonProcessingException{
-		
 		Map<String, String> myMap = new HashMap<String, String>();
 		myMap.put(key, value);
 		return objectMapper.writeValueAsString(myMap);
