b1add32f1564e9e81b1d914ace6a1c9b66bb9e34
Hemanth <hemanth@beehyv.com>
2015-10-08 13:59:15 +0530

    change in number search controller

--- src/main/java/com/imi/rest/controller/NumberSearchController.java
+++ src/main/java/com/imi/rest/controller/NumberSearchController.java
@@ -4,16 +4,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.imi.rest.model.GenericResponse;
+import com.imi.rest.model.Meta;
+import com.imi.rest.model.PurchaseDetails;
+
 @RestController
 public class NumberSearchController {
 
-	@RequestMapping("/PhoneNumber/{countryIsoCode}/{numberType}/{serviceType}/{pattern}")
-	public String numberSearchResponse(
+	@RequestMapping("/PhoneNumber/{countryIsoCode}/{providerId}/{numberType}/{serviceType}/{pattern}")
+	public GenericResponse numberSearchResponse(
 			@PathVariable("countryIsoCode") String countryIsoCode,
+			@PathVariable("providerId") String providerId,
 			@PathVariable("numberType") String numberType,
 			@PathVariable("serviceType") String serviceType,
 			@PathVariable("pattern") String pattern) {
-		return countryIsoCode;
+		
+		GenericResponse genResponse = new GenericResponse();
+		genResponse.setMeta(new Meta());
+		genResponse.setObject(new PurchaseDetails());
+		
+		return genResponse;
 	}
 
 }
